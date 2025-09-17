ad5d47da4274346c70978af08db3e95f90c92779
gregvp00 <gregvp002@gmail.com>
2025-09-17 18:37:15 +0200

    Update CV and email fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import Image from "next/image";
 import Nav from "@/components/Nav";
 import AppPreview from "@/components/AppPreview";
-import CodeEditor from "@/components/CodeEditor"; // <-- 1. Importa el componente de editor de código
-import AppTabs from "@/components/AppTabs"; // <-- 1. Importa el componente de pestañas
+import CodeEditor from "@/components/CodeEditor";
+import AppTabs from "@/components/AppTabs";
 import { projects } from "@/data/pageData";
 import { IconCode, IconUserCheck } from "@tabler/icons-react";
 import Carousel from "@/components/Carousel";
@@ -18,7 +18,56 @@ export default function Home() {
     dark: ["#000000", "#103d15", "#1f7a2a", "#2fb63f", "#56d364"],
   };
 
-  // --- 2. Define el contenido para las pestañas ---
+  const animatedRefs = useRef(new Map());
+
+  useEffect(() => {
+    const emailLink = document.getElementById("email-link");
+
+    const handleEmailClick = () => {
+      const usuario = emailLink.getAttribute("data-usuario");
+      const dominio = emailLink.getAttribute("data-dominio");
+      window.location.href = `mailto:${usuario}@${dominio}?subject=Consulta desde la web`;
+    };
+
+    if (emailLink) {
+      emailLink.addEventListener("click", handleEmailClick);
+    }
+
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.2,
+    };
+
+    const observerCallback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove("opacity-75", "grayscale");
+          entry.target.classList.add("opacity-100", "grayscale-0");
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions
+    );
+
+    animatedRefs.current.forEach((element) => {
+      if (element) {
+        observer.observe(element);
+      }
+    });
+
+    return () => {
+      if (emailLink) {
+        emailLink.removeEventListener("click", handleEmailClick);
+      }
+      observer.disconnect();
+    };
+  }, []);
+
   const CodeTabPage = () => (
     <div className="text-sm">
       <CodeEditor />
@@ -37,28 +86,6 @@ export default function Home() {
       content: <CodeTabPage />,
     },
   ];
-  // --- Fin de la definición ---
-
-  useEffect(() => {
-    const emailLink = document.getElementById("email-link");
-
-    const handleEmailClick = () => {
-      const usuario = emailLink.getAttribute("data-usuario");
-      const dominio = emailLink.getAttribute("data-dominio");
-      window.location.href = `mailto:${usuario}@${dominio}?subject=Consulta desde la web`;
-    };
-
-    if (emailLink) {
-      emailLink.addEventListener("click", handleEmailClick);
-    }
-
-    // Limpieza: elimina el event listener cuando el componente se desmonte
-    return () => {
-      if (emailLink) {
-        emailLink.removeEventListener("click", handleEmailClick);
-      }
-    };
-  }, []); // El array vacío asegura que este efecto se ejecute solo una vez
 
   return (
     <div>
@@ -100,10 +127,10 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 mx-8 lg:mx-20">
             {projects.map((data) => (
               <article
-                className="relative rounded-lg hover:-translate-y-1 lg:opacity-75 lg:grayscale lg:hover:grayscale-0 lg:hover:opacity-100 hover:p-0.75 shadow-xl transition-transform duration-300 overflow-hidden group"
                 key={data.id}
+                ref={(element) => animatedRefs.current.set(data.id, element)}
+                className="relative rounded-lg opacity-75 grayscale lg:hover:grayscale-0 lg:hover:opacity-100 hover:p-0.75 shadow-xl transition-transform duration-300 overflow-hidden group"
               >
-                {/* El fondo SVG animado */}
                 <div className="absolute -z-10 bg-[#8b3037] inset-0">
                   <AnimatedBlobBackground />
                 </div>
@@ -214,7 +241,7 @@ export default function Home() {
             </div>
             <div className="relative z-10 flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-6 my-6 mx-6">
               <Link
-                href="https://acrobat.adobe.com/id/urn:aaid:sc:EU:123b4dda-fe60-4e77-8348-974374fc767c"
+                href="https://acrobat.adobe.com/id/urn:aaid:sc:EU:ad63d264-d78b-432d-ac1f-ebe272684569"
                 className="relative [text-shadow:2px_2px_4px_rgba(0,0,0,1)] bg-black/80 rounded-lg p-2 text-xl py-2 px-4 text-white hover:-translate-y-1 transition-transform duration-200 shadow-lg shadow-black/70"
               >
                 Download my resume
@@ -237,60 +264,6 @@ export default function Home() {
           </div>
         </main>
         <footer className="grid grid-cols-2 justify-between items-center gap-80">
-          {/* <div className="border-1 border-gray-400 rounded-t-md p-3">
-            <GitHubCalendar username="gregvp00" theme={explicitTheme} />
-          </div>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-
-          <div className="h-10 w-10 bg-[#525f6f]"></div>
-          <div className="h-10 w-10 bg-[#8b3037]"></div>
-          <div className="h-10 w-10 bg-[#d8cfc0]"></div>
-          <div className="h-10 w-10 bg-[#adccc2]"></div> */}
-
           <div>
             <h3 className="">Location</h3>
             <h3 className="opacity-70">Valencia, Spain</h3>
